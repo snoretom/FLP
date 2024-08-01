526fed9f919699c2c9d50277f4c60b073cc7a5ba
moomHome <snoretom@naver.com>
2024-08-01 18:19:43 +0900

    asd

---
+++ FantasyLeague/fl_backend/src/main/java/com/fantasyleague/demo/FantasyleagueApplication.java
@@ -0,0 +1,13 @@
+package com.fantasyleague.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyleagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyleagueApplication.class, args);
+	}
+
+}
---
+++ FantasyLeague/fl_backend/src/test/java/com/fantasyleague/demo/FantasyleagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fantasyleague.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyleagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
